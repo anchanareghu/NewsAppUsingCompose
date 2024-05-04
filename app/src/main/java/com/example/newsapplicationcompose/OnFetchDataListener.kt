722c1d7fde79a0dcd93c4f0e47e74fad8f432c50
Anchana <anchanareghu20@gmail.com>
2024-05-04 20:48:23 +0530

    Commit to main

--- app/src/main/java/com/example/newsapplicationcompose/OnFetchDataListener.kt
+++ app/src/main/java/com/example/newsapplicationcompose/OnFetchDataListener.kt
@@ -3,6 +3,6 @@ package com.example.newsapplicationcompose
 import com.example.newsapplicationcompose.models.HeadLines
 
 interface OnFetchDataListener {
-    fun onFetchData(headLinesList: List<HeadLines?>?, message: String?)
+    fun onFetchData(newsList: List<HeadLines?>?, message: String?)
     fun onError(message: String?)
 }
